c5f8c1b815909d01bbac23f557ab98150dcb8c69
afifarn <afifr0837@gmail.com>
2019-10-17 23:13:59 +0700

    first commit

---
+++ src/main/java/PBOUlang_10116138_Latihan39_NilaiTerbesarDanTerkecil/Nilai.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116138_Latihan39_NilaiTerbesarDanTerkecil;
+
+/**
+ *
+ * @author Aradnu
+ */
+public class Nilai {
+    
+}
